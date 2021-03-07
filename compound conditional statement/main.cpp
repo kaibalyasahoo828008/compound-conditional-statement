d03a608f251a5c52ab37bb05a89181ada7a3162d
kaibalya sahoo <120CR0406@nitrkl.ac.in>
2021-03-07 21:09:46 +0530

    Initial Commit

---
+++ compound conditional statement/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  compound conditional statement
+//
+//  Created by KAIBALYA SAHOO on 16/12/1942 Saka.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
